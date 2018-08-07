6ea77c43e020862ce947c6aec8ee888ec25c5c91
dineshbalaji <dineshbalaji20@gmail.com>
2018-08-07 15:12:06 +0530

    Add files via upload
    webpack 4

--- webpack.config.js
+++ webpack.config.js
@@ -1,48 +1,29 @@
-var path = require('path');
-var webpack = require('webpack');
-
 module.exports = {
-    /*entry: [
-    path.join(__dirname, 'src/app.js'),
-    /!*'webpack/hot/dev-server',
-    'webpack-dev-server/client?http://localhost:7711/',*!/
-    ],*/
-    entry:{
-      iterator:path.join(__dirname, 'src/iterator.js'),
-      collections:path.join(__dirname, 'src/collections.js'),
-    },
-    output: {
-        path: __dirname + '/build',
-        filename: '[name].bundle.js',
-		publicPath: '/',
-    },
-    module: {
-        loaders: [
-            {
-                loader: 'babel-loader',
-                test: path.join(__dirname, 'src'),
-                query: {
-                  presets: 'es2015',
-                  plugins: [ "transform-runtime" ]
-                },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /(node_modules)/,
+        use: 'babel-loader',
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['react', 'env'],
+            /* plugins: ['@babel/plugin-transform-runtime'] */
+          }
+        }
+      }
+    ]
+  },
+  optimization: {
+    splitChunks: {
+        cacheGroups: {
+            commons: {
+                test: /[\\/]node_modules[\\/]/,
+                name: 'vendor',
+                chunks: 'initial'
             }
-        ]
-    },
-    plugins: [
-        // Avoid publishing files when compilation fails
-        new webpack.NoErrorsPlugin()/*,
-        new webpack.HotModuleReplacementPlugin()*/
-    ],
-    stats: {
-        // Nice colored output
-        colors: true
-    },
-    // Create Sourcemaps for the bundle
-    devtool: 'source-map',
-    /*devServer:  {
-      host: 'localhost',
-      port: 7711,
-      hot: false,
-      contentBase: __dirname + '/build'
-    }*/
+        }
+    }
+}
 };
